cf53c83284b6db6afb5eda11b99c4978b7c888b6
brad-stephen-shaw <bradley.stephen.shaw@outlook.com>
2024-03-07 21:28:49 +0000

    ReLU, change points, Fourier seasonality

--- feature_configs.py
+++ feature_configs.py
@@ -4,6 +4,7 @@ date_feature_definitions_monthly = {
     'trend_yr': lambda u: u.year,
     'trend_qtr': lambda u: u.quarter,
     'trend_mth': lambda u: u.month,
+    'trend_dim': lambda u: u.days_in_month,
     'season_qtr_cos': lambda u: np.cos(2 * np.pi * (u.quarter - 1) / 4),
     'season_qtr_sin': lambda u: np.sin(2 * np.pi * (u.quarter - 1) / 4),
     'season_mth_cos': lambda u: np.cos(2 * np.pi * (u.month - 1) / 12),
--- feature_engineering.py
+++ feature_engineering.py
@@ -53,13 +53,13 @@ def count_holidays_helper(u, region='GB'):
     return sum(y in hols for y in days)
 
 
-def count_bank_holidays(data, date_column, name='holiday_bank_count', proportion=False, append=True):
+def count_bank_holidays(data, date_column, name='holiday_bank_count', region='GB', proportion=False, append=True):
     '''
     Function to count bank holidays
     '''
     
     d = pd.DataFrame(index=data.index)
-    d[name] = data[date_column].apply(count_holidays_helper)
+    d[name] = data[date_column].apply(count_holidays_helper, region=region)
     
     if proportion:
         d[f'{name}_prop'] = d[name] / data[date_column].dt.days_in_month
@@ -70,3 +70,70 @@ def count_bank_holidays(data, date_column, name='holiday_bank_count', proportion
     else:
         return d
     
+    
+
+def create_relu(data, date_column, dates, append=True):
+    '''
+    Function to create a ReLU based on difference in months between
+    dates and data[date_column].
+    Returns a column for each date in dates.
+    '''
+    
+    d = pd.DataFrame()
+    
+    for date in dates:
+        
+        # get difference in months
+        delta = (data[date_column].dt.year * 12 + data[date_column].dt.month) - (date.year * 12 + date.month)
+        relu = np.maximum(0, delta)
+        
+        d = pd.concat([d, pd.DataFrame({f"trend_relu_{date.strftime('%Y%m')}": relu})], axis=1)
+        
+    
+    if append:
+        return pd.concat([data, d], axis=1)
+    
+    else:
+        return d
+    
+    
+def create_change_points(data, date_column, dates, append=True):
+    '''
+    Function to create a change point. Will be 1 where dates >= data[date_column].
+    Returns a column for each date in dates.
+    '''
+    
+    d = pd.DataFrame()
+    
+    for date in dates:
+        
+        # get difference in months
+        d = pd.concat(
+                [d, pd.DataFrame({f"trend_change_{date.strftime('%Y%m')}": (data[date_column] >= date) * 1})],
+                axis=1)
+        
+    
+    if append:
+        return pd.concat([data, d], axis=1)
+    
+    else:
+        return d
+    
+    
+def create_fourier_seasonality(data, periodicity, n_components, append=True):
+    '''
+    Function to create Fourier seasonality.
+    '''
+    
+    idx = np.arange(len(data))
+    d = pd.DataFrame()
+    
+    for n in range(1, n_components + 1):
+        d[f'season_fourier_cos_{n}'] = np.cos(2 * np.pi * n * idx / periodicity)
+        d[f'season_fourier_sin_{n}'] = np.sin(2 * np.pi * n * idx / periodicity)
+        
+    if append:
+        return pd.concat([data, d], axis=1)
+    
+    else:
+        return d  
